6f27fb074327f9f1cc23958a84a8cff3f9d510fe
Ferhat Karasu <74055034+ferhatkarasu@users.noreply.github.com>
2025-09-13 02:17:36 +0300

    Create caesar.java

---
+++ Projects/Caesar Cipher/caesar.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class caesar {
+
+public static final String alpha = "abcdefghijklmnopqrstuvwxyz";
+public static String encrypt(String message, int shiftKey){
+
+    message = message.toLowerCase();
+    String cipherText = "";
+
+    for (int i = 0; i < message.length(); i++) {
+
+        int charPosition = alpha.indexOf(message.charAt(i));
+        int keyVal= (shiftKey + charPosition) % 26;
+        char replaceVal = alpha.charAt(keyVal);
+        cipherText += replaceVal;
+    }
+    return cipherText;
+}
+
+public static void main(String [] args){
+    Scanner sc = new Scanner(System.in);
+    String message = new String();
+    int key=0;
+    System.out.print("Enter the String for Encryption: ");
+    message = sc.nextLine();
+
+    System.out.print("Enter the Shift Key: ");
+    key = sc.nextInt();
+
+    System.out.println("Encrpyted Message:"+ encrypt(message, key));
+}
+
+}
